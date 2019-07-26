46da63319d8c76079fa009389c5b5649ffa25f58
Aditya Vishwanathan <53296751+avish2206@users.noreply.github.com>
2019-07-27 02:40:23 +1000

    Add files via upload
    Generalized dynamic programming implementation for MATLAB

--- matlab/tsp_dynamic.m
+++ matlab/tsp_dynamic.m
@@ -4,7 +4,8 @@
 
 clc; clear; close all;
 
-% matrix    
+%% INPUT GRAPH HERE - DISTANCE MATRIX + STARTING NODE %%%%%%%%%%%%%%%%%%%%%
+% example matrix    
 %nodes:  1    2    3    4    5
 graph = [nan, 10,  8,   9,   7;    %1
          10,  nan, 10,  5,   6;    %2
@@ -13,76 +14,56 @@ graph = [nan, 10,  8,   9,   7;    %1
          7,   6,   9,   6,   nan]; %5
      
 starting = 1; % starting node
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-nodes = [1,2,3,4,5]; % array of nodes
+nodes = 1:length(graph); % array of nodes
 
-unvisited = setdiff(nodes,starting);
+unvisited = setdiff(nodes,starting); % set of nodes left to visit
 
-% last step
-dist1(1) = nan;
-for loop1 = unvisited
-    dist1(loop1) = graph(loop1,1); 
+dist = nan(length(nodes), length(nodes)); % distances to store in mem
+prev = nan(length(nodes), length(nodes)); % previous best nodes
+
+prev(:,1) = starting; % all final steps point to starting
+
+% for loop iterating through each decision (each level in the decision tree)
+for i = 1:size(dist,2)
+   if i==1
+      for loop = unvisited
+          dist(loop,i) = graph(loop,starting);
+      end
+   elseif i==size(dist,2)
+       dist(:,i) =dist(:,i-1)+graph(starting,:)';
+       min_dist = min(dist(:,i));
+       idx = find(dist(:,i)==min_dist);
+       route = starting;
+       next = prev(idx,i-1);
+       for j = i-2:-1:1
+           route = [route, next];
+           next = prev(next,j);
+       end
+       route = [route, next];
+       fprintf('Best Route: '); fprintf(repmat('%d ',1,length(nodes)+1),route);
+       fprintf('\nDistance: %d\n',min_dist);
+   else
+      for loop = unvisited
+          min_dist = inf;
+          idx = nan;
+          for j = setdiff(unvisited,loop)
+              temp = dist(j,i-1) + graph(loop,j);
+              visited = [];
+              for k = i-1:-1:1
+                  visited = [visited, prev(j,k)]; 
+              end
+              if sum(ismember(visited,loop))==0 && temp < min_dist
+                  min_dist = temp;
+                  idx = j;
+              end
+          end
+          dist(loop,i) = min_dist;
+          prev(loop,i) = idx;
+      end
+   end
 end
-
-% second last step
-dist2(1,:) = nan;
-for loop2 = unvisited
-    min_dist = inf;
-    for i = setdiff(unvisited,loop2)
-        dist = dist1(i) + graph(loop2,i);
-        if(dist < min_dist)
-           min_dist = dist;
-           idx = i;
-        end
-    end
-    dist2(loop2,1) = min_dist;
-    dist2(loop2,2) = idx;
-end
-
-% second last step
-dist3(1,:) = nan;
-for loop3 = unvisited
-    min_dist = inf;
-    for i = setdiff(unvisited,[loop3])
-        dist = dist2(i,1) + graph(loop3,i);
-        next = dist2(i,2);
-        if (dist < min_dist && next~=loop3)
-           min_dist = dist;
-           idx = i;
-        end
-    end
-    dist3(loop3,1) = min_dist;
-    dist3(loop3,2) = idx;
-end
-
-% third last step
-dist4(1) = nan;
-for loop4 = unvisited
-    min_dist = inf;
-    for i = setdiff(unvisited,[loop4])
-        dist = dist3(i,1) + graph(loop4,i);
-        next = dist3(i,2);
-        next2 = dist2(next,2);
-        if(dist < min_dist && next~=loop4 && next2~=loop4)
-           min_dist = dist;
-           idx = i;
-        end
-    end
-    dist4(loop4,1) = min_dist;
-    dist4(loop4,2) = idx;
-end
-
-% last step
-dist5 = dist4;
-dist5(2:end,1) = dist5(2:end,1) + graph(1,2:end)';
-min_dist = min(dist5(:,1));
-idx = find(dist5(:,1)==min_dist);
-next = dist4(idx,2);
-next2 = dist3(next,2);
-next3 = dist2(next2,2);
-
-fprintf('BEST ROUTE: %d %d %d %d %d %d\nDISTANCE: %d\n',1,idx,next,...
-          next2,next3,1,min_dist);
       
              
 
